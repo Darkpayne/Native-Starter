b75f821f55f01802710b1cf30da1460cc6a0a3f5
Darkpayne <joshuaclifford4336@yahoo.com>
2023-01-27 09:31:31 +0100

    Native Tailwind Set Up done

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="">
+      <Text className="text-red-500">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
